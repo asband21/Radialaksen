f19901316650209f635863b916460b9a62a723df
asbjørn <qw41ha@student.aau.dk>
2022-03-08 11:37:38 +0100

    tilføget en start classe

--- opgaver/oob/oob.py
+++ opgaver/oob/oob.py
@@ -1,1 +1,10 @@
-print("hej verden top oob")
+class per:
+    def __init__(self, n , strin):
+        self.n = n
+        self.strin = strin
+    def prin_n(self):
+        for i in range(self.n):  
+            print(self.strin)
+ob_print = per(55 , "hej med dig")
+ob_print.prin_n()
+
