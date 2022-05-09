2c9f2e6750f22faa90b6735a317bf3a921b6986d
TobiasBJensen <93526045+TobiasBJensen@users.noreply.github.com>
2022-05-09 08:22:47 +0200

    changed pins

--- forvert_kinamatik/robot_simulatsion/J_testing.m
+++ forvert_kinamatik/robot_simulatsion/J_testing.m
@@ -28,15 +28,15 @@ clc; clear; close all;
 %Husk at port nummet skal ændres til jeres port eks"COM14"
 a = arduino('COM3', 'Uno', 'Libraries', 'Servo');
 
-wrist = servo(a, 'D9'); %, 'MinPulseDuration', 1e-3, 'MaxPulseDuration', 2e-3);
-roll = servo(a, 'D8'); %, 'MinPulseDuration', 9e-4, 'MaxPulseDuration', 2e-3);
-elbow = servo(a, 'D7');%, 'MinPulseDuration', 1e-3, 'MaxPulseDuration', 2e-3);
+wrist = servo(a, 'D11'); %, 'MinPulseDuration', 1e-3, 'MaxPulseDuration', 2e-3);
+roll = servo(a, 'D10'); %, 'MinPulseDuration', 9e-4, 'MaxPulseDuration', 2e-3);
+elbow = servo(a, 'D9');%, 'MinPulseDuration', 1e-3, 'MaxPulseDuration', 2e-3);
 shoulder = servo(a, 'D6');%, 'MinPulseDuration', 1e-3, 'MaxPulseDuration', 2e-3);
 base = servo(a, 'D5');%, 'MinPulseDuration', 1e-3, 'MaxPulseDuration', 2e-3);
 
 %% Vertical
-writePosition(wrist,1);
-writePosition(roll,1);
+writePosition(wrist,0.5);
+writePosition(roll,0.5);
 writePosition(elbow,0.5);
 writePosition(shoulder,0.5);
-writePosition(base,0); 
+writePosition(base,0.5); 
