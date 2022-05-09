0bd6bd28f7cdae3fe4c34024286be5f3e4136971
asbjørn <qw41ha@student.aau.dk>
2022-05-09 09:37:42 +0200

    ny forvert kinamatfil

--- forvert_kinamatik/robot_simulatsion/a_robsimulator.m
+++ forvert_kinamatik/robot_simulatsion/a_robsimulator.m
@@ -16,11 +16,11 @@ angle_cov_radiens = @(angle) (angle*pi)/180;
 0,0,(177+180)/2;
 0,(7.5+10)/2,(178+180)/2 
 %}  
-degree = [0,0,180,57.2958;
-        90,12,180,45.2637+180;
-        90,7.5,180,-88.8085-90;
-        0,0,180.5,0;
-        90,-7.5,180,45.8366-90]
+degree = [0,0,180,80 ;%98; %80;
+          0,12,180, 45+180; %45.2637+180;
+          0,7.5,180,180; %-88.8085+270;
+          0,0,180.5,0;
+          0,-7.5,180,45.8366-90]
 
 angle_radiens = degree;
 for i = 1 : 1 :5
@@ -30,7 +30,7 @@ end
 
 
 %% set Position 
-    arm_postion(:)= J_Forward_kinematic(angle_radiens(1,1),angle_radiens(2,1),angle_radiens(3,1),angle_radiens(4,1),angle_radiens(5,1));
+    arm_postion(:)= J_Forward_kinematic(degree(1,1),degree(2,1),degree(3,1),degree(4,1),degree(5,1));
         x = arm_postion(1)
         y = arm_postion(2)
         z = arm_postion(3)
@@ -40,31 +40,67 @@ end
     robot_config = ConfigStruct(angle_radiens(:,1).', end_effector);
     
 %% Ploting
-for j = 1 :1: 5
+sim_ang = []
+for j = 1 :1 : 5
     degree(j,:) = bouns(degree(j,:));
-    degree(j,1) = angle_cov_radiens(degree(j,1)+degree(j,4));
+    if j == 3
+        sim_ang(j,1) = angle_cov_radiens(degree(j,3)+degree(j,2)-degree(j,1)+degree(j,4));
+    else
+        sim_ang(j,1) = angle_cov_radiens(degree(j,1)+degree(j,4));
+    end
 end
-robot_config = ConfigStruct(degree, end_effector);
+robot_config = ConfigStruct(sim_ang, end_effector);
 show(robot,robot_config,'PreservePlot',false,'Frames','off');
 %axis([-1 1 -1 1 -0.1 0.5]);
 poseNow = getTransform(robot, robot_config, 'end_point') 
-asb_for = J_Forward_kinematic(degree(1),degree(2)+pi*0.5,degree(3),degree(4),degree(5))/1000   
-poseNow = getTransform(robot, robot_config, 'end_point')
+asb_for = J_Forward_kinematic(degree(1),degree(2),degree(3),degree(4),degree(5))/1000   
+poseNow = getTransform(robot, robot_config, 'end_point');
 
-%{
-for i = 1:3:360
-    for j = 1 :1: 5
-        degree(j,1) = i;
-        degree(j,:) = bouns(degree(j,:));
-        degree(j,1) = angle_cov_radiens(degree(j,1)+degree(j,4));
-    end
+%
+x_asb = [];
+y_asb = [];
+x_asb = [];
+x_sim = [];
+y_sim = [];
+z_sim = [];
+for i  = 0:20:180
+for v2 = 0:20:180
+for v3 = 0:45:180
+for v4 = 0:45:180
+for v5 = 0:45:180
     i = i
-    asb_for = J_Forward_kinematic(degree(1),degree(2),degree(3),degree(4),degree(5))
-    robot_config = ConfigStruct(degree, end_effector);
-    pause(0.01)
+    list_isk = [i v2 v3 v4 v5]
+    for j = 1 :1: 5
+        degree(j,1) = list_isk(j);
+        degree(j,:) = bouns(degree(j,:));
+        if j == 3
+            sim_ang(j,1) = angle_cov_radiens(degree(j,3)+degree(j,2)-degree(j,1)+degree(j,4));
+        else
+            sim_ang(j,1) = angle_cov_radiens((degree(j,1))+degree(j,4));
+        end
+    end
+    asb_for = J_Forward_kinematic(degree(1),degree(2),degree(3),degree(4),degree(5))/1000;
+    robot_config = ConfigStruct(sim_ang, end_effector);
+    %pause(0.01)
     show(robot,robot_config,'PreservePlot',false,'Frames','off');
     %axis([-1 1 -1 1 -0.1 0.5]);
-    poseNow = getTransform(robot, robot_config, 'end_point') 
+    poseNow = getTransform(robot, robot_config, 'end_point');
+    jjjj = size(x_asb);
+    x_asb(jjjj(2)+1) = asb_for(1);
+    y_asb(jjjj(2)+1) = asb_for(2);
+    z_asb(jjjj(2)+1) = asb_for(3);
+    x_sim(jjjj(2)+1) = poseNow(1,4);
+    y_sim(jjjj(2)+1) = poseNow(2,4);
+    z_sim(jjjj(2)+1) = poseNow(3,4);
 end
-%}
+end
+end 
+end
+end
 
+tab = [x_asb;y_asb;z_asb;x_sim;y_sim;z_sim;x_asb-x_sim;y_asb-y_sim;z_asb-z_sim].'
+plot3(x_asb,y_asb,z_asb,x_sim,y_sim,z_sim,x_asb-x_sim,y_asb-y_sim,z_asb-z_sim)
+xlim([-0.350 0.350])
+ylim([-0.350 0.350])
+zlim([-0.5 0.700])
+%}
