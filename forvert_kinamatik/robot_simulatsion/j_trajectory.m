03e4efae6a0cdd9d4f223d527092a70c589ae068
Jakob-Jorgensen <jakob.jorgensen01@gmail.com>
2022-05-13 14:19:00 +0200

    trjectory update

--- forvert_kinamatik/robot_simulatsion/j_trajectory.m
+++ forvert_kinamatik/robot_simulatsion/j_trajectory.m
@@ -1,9 +1,6 @@
-function done = j_trajectory(t_1,t_2,n)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here  
+function f = j_trajectory(t_1,t_2)
 r_1 = t_2(1)-t_1(1);
 r_2=t_2(2)-t_1(2);
 r_3=t_2(3)-t_1(3);
 f= @(x)[t_1(1)+x*r_1,t_1(2)+x*r_2,t_1(3)+x*r_3];
-done=f(n);
 end
