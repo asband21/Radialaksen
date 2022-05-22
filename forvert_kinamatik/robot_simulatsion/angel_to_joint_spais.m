3106e2ec541f9748e23ab2a247513695e21c3fe5
asbjørn <qw41ha@student.aau.dk>
2022-05-22 15:10:53 +0200

    rm things

--- forvert_kinamatik/robot_simulatsion/angel_to_joint_spais.m
+++
@@ -1,4 +0,0 @@
-function output = angel_to_joint_spais(list)
-    list = bouns(list);
-    output = (list(1)*list(2))/180+list(1);
-end
--- forvert_kinamatik/robot_simulatsion/bouns.m
+++
@@ -1,4 +0,0 @@
-function output = bouns(list)
-    list(1) = clamp(list(1),list(2),list(3)+list(2));
-    output = list;
-end
